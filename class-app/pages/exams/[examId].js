ad97616f11ce5aabc0b11b020431f121aec9e3ff
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-04-30 10:49:23 -0500

    Started solve exams page

--- class-app/pages/exams/[examId].js
+++ class-app/pages/exams/[examId].js
@@ -1,13 +1,27 @@
 import { useRouter } from 'next/router'
 //import { useRef } from 'react'
 
-function SolveExamsPage(){
+function SolveExamsPage({ data }){
     //const reactComp = useRef();
-    const router = useRouter();
-    const testId = router.query.testId;
+    //const router = useRouter();
+    
+    return  <div>
+        <h1>Solve Exams Page</h1>
+        <p>{data.name}</p>
+    </div>
+    
+}
 
-    return  <h1>Solve Exams page</h1>
-        
+export async function getServerSideProps({ query }) {
+    const examId = query.examId
+    // const router = useRouter()
+    // let examId = router.query.exams
+    // const router = useRouter()
+    // const examId = router.query.exams;
+    const res = await fetch("http://localhost:3000/api/exams/" + examId)
+    const data = await res.json()
+    console.log(data)
+    return { props: {data} }
 }
 
 export default SolveExamsPage;
--- class-app/pages/exams/exams-list.js
+++ class-app/pages/exams/exams-list.js
@@ -1,30 +1,39 @@
 import Link from "next/link" // para que no se refresque la pagina
-
-let handleClick =  e => {
-    console.log("Doest nothing yet. Needs to show the exam")
-}
+import { useRouter } from "next/router";
+import { useRef } from "react";
 
 
 function Card(props) {
+    const router = useRouter();
+    const idRef = useRef()
+
+    let handleClick = e => {
+        console.log(idRef.current.value)
+        router.push("/exams/" + idRef.current.value);
+        //console.log("Doest nothing yet. Needs to show the exam")
+    }
+
     //console.log(props.name)
     return <div className="card border-success mb-3" style={{maxWidth: "18rem"}}>
         <div className="card-header">{props.subject}</div>
         <div className="card-body text-success">
         <h5 className="card-title">{props.name}</h5>
         <p className="card-text">{props.description}</p>
-        <p id="examId" style={{display: "none"}}>{props.id}</p>
-        <button onClick={handleClick} type="button" className="btn btn-success">Empezar</button>
+        <input type="text" ref={idRef} style={{display : "none"}} value={props.id} readOnly/>
+        {/* <p id="examId" style={{display: "none"}}>{props.id}</p> */}
+        <button onClick={handleClick} type="button" className="btn btn-success" value={props.id}>Empezar</button>
         </div>
   </div>
 }
  
 function ExamsList({ data }){
+
     return <div>
         <h1 className='display-3 mx-auto text-center'>Exams List</h1>
         <Link  href="/exams">Regresar</Link>
         {data.exams.map((item) => {
             console.log(item.name)
-            return <Card key={item.name} name={item.name} description={item.description} subject={item.subject}/>
+            return <Card key={item.name} name={item.name} description={item.description} subject={item.subject} id={item.id}/>
         })}
     </div>
         
