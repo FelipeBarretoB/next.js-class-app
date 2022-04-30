c2833d6d1ea58654c2b441b908a29bbf03e389b5
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-04-30 12:35:26 -0500

    Fixed absolute routes

--- class-app/pages/api/exams/[returnExam].js
+++ class-app/pages/api/exams/[returnExam].js
@@ -3,7 +3,7 @@ const fs = require('fs')
 export default function handler (req, res) {
      const examId = req.query.returnExam
      let found = false
-     let data = JSON.parse(fs.readFileSync('C:/Users/USUARIO/Desktop/Escritorio2/Proyectos/Universidad/NextJS/next.js-class-app/class-app/database/exams.json', 'utf8')).exams;
+     let data = JSON.parse(fs.readFileSync('database/exams.json', 'utf8')).exams;
      let exam = data.find((ex) => ex.id == examId)
      //console.log(examId)
      //console.log(exam.name)
