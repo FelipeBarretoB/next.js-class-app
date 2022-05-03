2aef508b6fd8e6904750ec5895e3e8865191eb94
cosmeticlizard <pipe.barreto07@gmail.com>
2022-05-02 23:18:44 -0500

    se comienza más no termina el resolver examenes con la db

--- class-app/pages/api/exams/[returnExam].js
+++ class-app/pages/api/exams/[returnExam].js
@@ -1,12 +1,17 @@
 const fs = require('fs')
+import db from '../../../util/database'
 
-export default function handler (req, res) {
+export default  async function handler (req, res) {
      const examId = req.query.returnExam
-     let found = false
-     let data = JSON.parse(fs.readFileSync('database/exams.json', 'utf8')).exams;
-     let exam = data.find((ex) => ex.id == examId)
+     /*let data = JSON.parse(fs.readFileSync('database/exams.json', 'utf8')).exams;
+     let exam = data.find((ex) => ex.id == examId)*/
+     let exam = await db.query('select e.* from exam e where e.id=$1',[examId])
+     let question = await db.query('select q.* from exam e, question q where e.id=q.id and e.id=$1',[examId])
+     let answer = await db.query('select a.* from answer a, question q where q.questionid=a.questionid and q.id=$1',[examId])
+     
+     
      //console.log(examId)
-     //console.log(exam.name)
+     //console.log(exam)
      //console.log(data)
-     res.send(exam)
+     res.send([exam.rows,question.rows, answer.rows])
  }
--- class-app/pages/exams/[examId].js
+++ class-app/pages/exams/[examId].js
@@ -3,7 +3,11 @@ import { useState } from 'react'
 //import { useRef } from 'react'
 
 function SolveExamsPage({ data }){
-    let questions = data.questions
+    //console.log(data)
+    let questions = data[1]
+    let answer =data[2]
+    
+    console.log(questions[0].questions)
     const [currentQuestion, setCurrentQuestion] = useState(0);
 
     let handlePrevious = () => {
@@ -26,16 +30,16 @@ function SolveExamsPage({ data }){
     
     return <div>
         <h1>Solve Exams Page</h1>
-        <h2>{data.name}</h2>
+        <h2>{data[0].name}</h2>
         
         <div>
             <h3>Question {currentQuestion + 1} of {questions.length}</h3>
             <div>
-                {questions[currentQuestion].question}
+                {questions[currentQuestion].questions}
             </div>
         </div>
         <div>
-            {questions[currentQuestion].answers.map((answer, index) => (
+            {answer.map((answer, index) => (
                 <div key={index} className="form-check">
                     <input type="radio" className="form-check-input" name={answer.answer}/>
                     <label> {answer.id})&nbsp;&nbsp;&nbsp; {answer.answer}</label>
@@ -43,8 +47,8 @@ function SolveExamsPage({ data }){
             ))}
         </div>
         
-        <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-            <div class="btn-group me-2" role="group" aria-label="First group">
+        <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+            <div className="btn-group me-2" role="group" aria-label="First group">
                 <button type="button" onClick={handlePrevious} className="btn btn-primary">Previous</button>
                 <button type="button" onClick={ () => {handleNum(0)}} className="btn btn-primary">1</button>
                 <button type="button" onClick={ () => {handleNum(1)}} className="btn btn-primary">2</button>
@@ -66,7 +70,7 @@ export async function getServerSideProps({ query }) {
     // const examId = router.query.exams;
     const res = await fetch("http://localhost:3000/api/exams/" + examId)
     const data = await res.json()
-    console.log(data)
+    //console.log(data)
     return { props: {data} }
 }
 
