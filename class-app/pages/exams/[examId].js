ff7a426ba69e5344a5e9f40c463a7ab5f7541a7c
G-Delgado <gabriel.delgado1@u.icesi.edu.co>
2022-05-06 18:30:09 -0500

    Solved exams compelte

--- class-app/pages/exams/[examId].js
+++ class-app/pages/exams/[examId].js
@@ -10,7 +10,32 @@ function SolveExamsPage({ data }){
     let answer =data[2]
     
     //console.log(questions[0].questions)
+    const [selectedOptions, setSelectedOptions] = useState([]);
     const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [score, setScore] = useState(0);
+    const [showScore, setShowScore] = useState(0)
+
+    let handleSubmit = () => {
+        //console.log(questions)
+        let newScore = 0;
+        for (let i = 0; i < questions.length; i++) {
+            
+            // I feel there's something wrong. But it works. So all good
+            let correctId = questions[i].correctanswer;
+            // console.log(answer[i])
+            // console.log(selectedOptions[i]?.answerByUser)
+            // console.log(correctId)
+            answer[i].map(
+                (answer) => 
+                    (answer.id == correctId) &&
+                    answer.anwers === selectedOptions[i]?.answerByUser &&
+                    (newScore += 1)
+            );
+        }
+        setScore(newScore)
+        //console.log(newScore)
+        setShowScore(true);
+    }
 
     let handlePrevious = () => {
         const prevQuestion = currentQuestion - 1;
@@ -31,41 +56,62 @@ function SolveExamsPage({ data }){
         router.push("/home")
     }
 
+    let handleAnswerOption = (answer) => {
+        setSelectedOptions([
+            (selectedOptions[currentQuestion] = {answerByUser: answer})
+        ])
+        setSelectedOptions([...selectedOptions]);
+        console.log(selectedOptions)
+    }
+
     //const reactComp = useRef();
     //const router = useRouter();
     
     return <div>
-        <h1>Solve Exams Page</h1>
-        <h2>{data[0].name}</h2>
-        
-        <div>
-            <h3>Question {currentQuestion + 1} of {questions.length}</h3>
+        {
+            showScore ? ( 
             <div>
-                {questions[currentQuestion].questions}
+                <h1 className="text-3xl font-semibold text-center text-black">You scored {score} out of {questions.length}</h1>
+                <button onClick={leaveExam}>Salir del examen</button>
             </div>
-        </div>
-        <div>
-            {answer[currentQuestion].map((answer, index) => (
-                
-                <div key={index} className="form-check">
-                    <input type="radio" className="form-check-input" name={answer.anwers}/>
-                    <label> {answer.id})&nbsp;&nbsp;&nbsp; {answer.anwers}</label>
+            ) :
+            (<div>
+                <h1>Solve Exams Page</h1>
+                <h2>{data[0].name}</h2>
+                <div>
+                    <h3>Question {currentQuestion + 1} of {questions.length}</h3>
+                    <div>
+                        {questions[currentQuestion].questions}
+                    </div>
                 </div>
-            ))}
-        </div>
-        
-        <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
-            <div className="btn-group me-2" role="group" aria-label="First group">
-                <button type="button" onClick={handlePrevious} className="btn btn-primary">Previous</button>
-                <button type="button" onClick={ () => {handleNum(0)}} className="btn btn-primary">1</button>
-                <button type="button" onClick={ () => {handleNum(1)}} className="btn btn-primary">2</button>
-                <button type="button" onClick={ () => {handleNum(2)}} className="btn btn-primary">3</button>
-                <button type="button" onClick={ () => {handleNum(3)}} className="btn btn-primary">4</button>
-                <button type="button" onClick={ () => {handleNum(4)}} className="btn btn-primary">5</button>
-                <button type="button" onClick={handleNext} className="btn btn-primary">Next</button>
-            </div>
-        </div>
-        <button onClick={leaveExam}>Salir del examen</button>
+                <div>
+                    {answer[currentQuestion].map((answer, index) => (
+                        
+                        <div key={index} className="form-check">
+                            <input type="radio" className="form-check-input" name={answer.anwers} onChange={(e) => handleAnswerOption(answer.anwers)}
+                            checked={answer.anwers === selectedOptions[currentQuestion]?.answerByUser}/>
+                            <label> {answer.id})&nbsp;&nbsp;&nbsp; {answer.anwers}</label>
+                        </div>
+                    ))}
+                </div>
+                
+                <div className="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                    <div className="btn-group me-2" role="group" aria-label="First group">
+                        <button type="button" onClick={handlePrevious} className="btn btn-primary">Previous</button>
+                        <button type="button" onClick={ () => {handleNum(0)}} className="btn btn-primary">1</button>
+                        <button type="button" onClick={ () => {handleNum(1)}} className="btn btn-primary">2</button>
+                        <button type="button" onClick={ () => {handleNum(2)}} className="btn btn-primary">3</button>
+                        <button type="button" onClick={ () => {handleNum(3)}} className="btn btn-primary">4</button>
+                        <button type="button" onClick={ () => {handleNum(4)}} className="btn btn-primary">5</button>
+                        <button type="button" onClick={currentQuestion + 1 === questions.length ? handleSubmit : handleNext} className="btn btn-primary">
+                        {currentQuestion + 1 === questions.length ? "Submit" : "Next"}
+                        </button>
+                    </div>
+                </div>
+                <button onClick={leaveExam}>Salir del examen</button>
+            </div>)
+        }
+
     </div>
     
 }
